dc86c5ef6d5d4bfbf156cdac0483ffaa3cf8bd50
shakthinr <shakthinaarayanan@gmail.com>
2023-08-06 18:08:02 +0530

    Fonts Configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
+import { Inter } from 'next/font/google'
 import "@/styles/global.css"
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>
+            <body  className={inter.className}>
                 <Navbar />
                 {children}
                 <Footer/>
